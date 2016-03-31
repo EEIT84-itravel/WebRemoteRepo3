bdafcb5703f42addf4df43f39efceda045629e20
fox <fox@III-EEIT>
2016-03-31 12:21:21 +0800

    新增由我的行程匯入遊記相關功能
    add FromTripJounalServlet.java,FromTripToJournal.jsp
    modify NewJournalServlet.java,TripService.java,
    TripDAOHibernate.java-新增select方法,
    NewJournal.jsp,NewTrip.jsp,WriteTrip.jsp-修改<select>參數

--- ItravleWeb/src/main/java/_02_TripAndJournal/model/TripService.java
+++ ItravleWeb/src/main/java/_02_TripAndJournal/model/TripService.java
@@ -1,17 +1,36 @@
 package _02_TripAndJournal.model;
 
+import java.util.List;
+
 import _02_TripAndJournal.model.dao.TripDAOHibernate;
 
 public class TripService {
-private TripDAOHibernate tripDAOHibernate;
-	
-	public TripVO insert(TripVO tripVO){
-		TripVO result=null;
-		if(tripVO!=null){
-			tripDAOHibernate=new TripDAOHibernate();
-			result=tripDAOHibernate.insert(tripVO);
+	private TripDAOHibernate tripDAOHibernate;
+
+	public TripVO insert(TripVO tripVO) {
+		TripVO result = null;
+		if (tripVO != null) {
+			tripDAOHibernate = new TripDAOHibernate();
+			result = tripDAOHibernate.insert(tripVO);
 		}
 		return result;
 	}
 
+	public List<TripVO> selectFromMember(Integer memberId) {
+		List<TripVO> result = null;
+		if (memberId >= 0) {
+			tripDAOHibernate = new TripDAOHibernate();
+			result = tripDAOHibernate.selectFromMember(memberId);
+		}
+		return result;
+	}
+
+	public TripVO select(Integer tripId) {
+		TripVO result = null;
+		if (tripId >= 0) {
+			tripDAOHibernate = new TripDAOHibernate();
+			result = tripDAOHibernate.select(tripId);
+		}
+		return result;
+	}
 }
