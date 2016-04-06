4895a530f6e8f0072f05ea544c1488bfc9c4dd26
fox <fox@III-EEIT>
2016-04-06 09:03:52 +0800

    modify gitignore

---
+++ ItravleWeb/src/main/java/_02_TripAndJournal/model/LabelTripAndJournalVO.java
@@ -0,0 +1,57 @@
+package _02_TripAndJournal.model;
+
+
+import java.io.Serializable;
+
+
+public class LabelTripAndJournalVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	// 編號(reference_no)
+	private Integer labelDetailId;
+	// 內容編號(reference_type)
+	private String referenceType;
+	// 參照編號(reference_type)
+	private Integer referenceNo;
+	// 標籤編號(label_id)
+	private String labelId;
+
+	public Integer getLabelDetailId() {
+		return labelDetailId;
+	}
+
+	public void setLabelDetailId(Integer labelDetailId) {
+		this.labelDetailId = labelDetailId;
+	}
+
+	public String getReferenceType() {
+		return referenceType;
+	}
+
+	public void setReferenceType(String referenceType) {
+		this.referenceType = referenceType;
+	}
+
+	public Integer getReferenceNo() {
+		return referenceNo;
+	}
+
+	public void setReferenceNo(Integer referenceNo) {
+		this.referenceNo = referenceNo;
+	}
+
+	public String getLabelId() {
+		return labelId;
+	}
+
+	public void setLabelId(String labelId) {
+		this.labelId = labelId;
+	}
+
+	@Override
+	public String toString() {
+		return "LabelTripAndJournalVO [labelDetailId=" + labelDetailId
+				+ ", referenceType=" + referenceType + ", referenceNo="
+				+ referenceNo + ", labelId=" + labelId + "]";
+	}
+
+}
---
+++ ItravleWeb/src/main/java/_02_TripAndJournal/model/TransVO.java
@@ -0,0 +1,35 @@
+package _02_TripAndJournal.model;
+
+import java.io.Serializable;
+
+public class TransVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	// 交通編號
+	private Integer transId;
+	// 交通方式
+	private String transFormId;
+
+	public Integer getTransId() {
+		return transId;
+	}
+
+	public void setTransId(Integer transId) {
+		this.transId = transId;
+	}
+
+	public String getTransFormId() {
+		return transFormId;
+	}
+
+	public void setTransFormId(String transFormId) {
+		this.transFormId = transFormId;
+	}
+
+	@Override
+	public String toString() {
+		return "TransVO [transId=" + transId + ", transFormId=" + transFormId
+				+ "]";
+	}
+
+}
